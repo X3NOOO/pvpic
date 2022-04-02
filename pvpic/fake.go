788c5501ee245b7c8b57c021ba91e91464d9aa6b
X3NO <X3NO@disroot.org>
2022-04-02 15:16:45 +0200

    added writing faked data

--- pvpic/fake.go
+++ pvpic/fake.go
@@ -2,17 +2,18 @@ package pvpic
 
 import (
 	"bytes"
+	"bufio"
 
 	"github.com/sfomuseum/go-exif-update"
 )
 
 func Fake(img []byte, model map[string]interface{})([]byte, error){
-	var faked []byte
+	var faked bytes.Buffer
 	var img_reader = bytes.NewReader(img)
 
-	var faked_writer = bytes.NewBuffer(faked)
+	var faked_writer = bufio.NewWriter(&faked)
 	
 	update.UpdateExif(img_reader, faked_writer, model)
 
-	return faked, nil
+	return faked.Bytes(), nil
 }
