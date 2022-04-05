7e80746d4b70ecdf3607753b7d5ad5b6b9e809c2
X3NO <X3NO@disroot.org>
2022-04-05 23:10:39 +0200

    backup

--- pvpic/fake.go
+++ pvpic/fake.go
@@ -18,7 +18,7 @@ func Fake(img []byte, model map[string]interface{}, current_time bool) ([]byte, 
 	if(current_time){
 		// get current timestamp in yyyy:mm:dd hh:mm:ss.sss
 		current_time := time.Now()
-		layout := "2006:01:02 3:4:5:06"
+		layout := "2006:01:02 03:04:05"
 		timestamp := current_time.Format(layout)
 		
 		// change model to add current timestamp
@@ -26,6 +26,12 @@ func Fake(img []byte, model map[string]interface{}, current_time bool) ([]byte, 
 		model["DateTimeOriginal"] = timestamp
 		model["DateTime"] = timestamp
 		model["PreviewDateTime"] = timestamp
+	// } else {
+		//// change model to add 00 timestamp
+		// model["DateTimeDigitized"] = "0000:00:00 00:00:00"
+		// model["DateTimeOriginal"] = "0000:00:00 00:00:00"
+		// model["DateTime"] = "0000:00:00 00:00:00"
+		// model["PreviewDateTime"] = "0000:00:00 00:00:00"
 	}
 
 	err := update.UpdateExif(img_reader, faked_writer, model)
